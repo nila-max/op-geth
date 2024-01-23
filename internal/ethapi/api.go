f76ba4b5b71514eae0b558baff738465ce7fbc78
wwqicode-blkchain <148736937+wwqicode-blkchain@users.noreply.github.com>
2024-01-23 15:37:46 +0800

    R4R: fix metatx estimateGas  (#33)
    * R4R: fix calculate sponsor amount while estimateGas with metatx
    
    * R4R: get min(sponsorAmount, sponsorBalance) while estimateGas with metatx
    
    * calculate sponsor percent amount with feeCap * hi
    
    * fix if tx.value equals to from balance, its valid
    
    ---------
    
    Co-authored-by: wwq <wwq@wwqdeMacBook-Pro.local>

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1232,21 +1232,25 @@ func DoEstimateGas(ctx context.Context, b Backend, args TransactionArgs, blockNr
 		if err != nil {
 			return 0, err
 		}
-		if metaTxParams != nil {
-			sponsorAmount, _ := types.CalculateSponsorPercentAmount(metaTxParams, feeCap)
-			sponsorBalance := state.GetBalance(metaTxParams.GasFeeSponsor)
-			if sponsorBalance.Cmp(sponsorAmount) <= 0 {
-				return 0, types.ErrSponsorBalanceNotEnough
-			}
-			balance = new(big.Int).Add(balance, sponsorAmount)
-		}
+
 		available := new(big.Int).Set(balance)
 		if args.Value != nil {
-			if args.Value.ToInt().Cmp(available) >= 0 {
+			if args.Value.ToInt().Cmp(available) > 0 {
 				return 0, core.ErrInsufficientFundsForTransfer
 			}
 			available.Sub(available, args.Value.ToInt())
 		}
+
+		if metaTxParams != nil {
+			sponsorAmount, _ := types.CalculateSponsorPercentAmount(metaTxParams, new(big.Int).Mul(feeCap, new(big.Int).SetUint64(hi)))
+			sponsorBalance := state.GetBalance(metaTxParams.GasFeeSponsor)
+			if sponsorAmount.Cmp(sponsorBalance) < 0 {
+				available.Add(available, sponsorAmount)
+			} else {
+				available.Add(available, sponsorBalance)
+			}
+		}
+
 		allowance := new(big.Int).Div(available, feeCap)
 
 		// If the allowance is larger than maximum uint64, skip checking
@@ -1327,21 +1331,23 @@ func calculateGasWithAllowance(ctx context.Context, b Backend, args TransactionA
 	if err != nil {
 		return 0, err
 	}
+
+	available := new(big.Int).Set(balance)
+	if args.Value != nil {
+		if args.Value.ToInt().Cmp(available) > 0 {
+			return 0, core.ErrInsufficientFundsForTransfer
+		}
+		available.Sub(available, args.Value.ToInt())
+	}
 	if metaTxParams != nil {
 		feeCap := new(big.Int).Mul(gasPriceForEstimate, big.NewInt(0).SetUint64(gasCap))
 		sponsorAmount, _ := types.CalculateSponsorPercentAmount(metaTxParams, feeCap)
 		sponsorBalance := state.GetBalance(metaTxParams.GasFeeSponsor)
-		if sponsorBalance.Cmp(sponsorAmount) <= 0 {
-			return 0, types.ErrSponsorBalanceNotEnough
+		if sponsorAmount.Cmp(sponsorBalance) < 0 {
+			available.Add(available, sponsorAmount)
+		} else {
+			available.Add(available, sponsorBalance)
 		}
-		balance = new(big.Int).Add(balance, sponsorAmount)
-	}
-	available := new(big.Int).Set(balance)
-	if args.Value != nil {
-		if args.Value.ToInt().Cmp(available) >= 0 {
-			return 0, core.ErrInsufficientFundsForTransfer
-		}
-		available.Sub(available, args.Value.ToInt())
 	}
 
 	allowanceGas := new(big.Int).Div(available, gasPriceForEstimate)
